a14bb1473e993ee41e2edca54a08bf7b313ccab5
Anmol Srivastava <106250049+its-anmol@users.noreply.github.com>
2023-12-14 23:49:43 +0530

    Time: 21 ms (22.67%) | Memory: 13.2 MB (46.93%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
-    int numSpecial(std::vector<std::vector<int>>& mat) {
-        int specials = 0;
-
-        for (int i = 0; i < mat.size(); i++) {
-            int index = checkRow(mat, i);
-            if (index >= 0 && checkColumn(mat, i, index))
-                specials++;
-        }
-
-        return specials;
-    }
-
-private:
-    int checkRow(std::vector<std::vector<int>>& mat, int i) {
-        int index = -1;
-        for (int j = 0; j < mat[0].size(); j++) {
-            if (mat[i][j] == 1) {
-                if (index >= 0)
-                    return -1;
-                else
-                    index = j;
+    int numSpecial(vector<vector<int>>& mat) {
+        int row=mat.size();
+        int col=mat[0].size();
+        vector<int> rowones(row,0);
+        vector<int> colones(col,0);
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(mat[i][j]==1){
+                    rowones[i]++;
+                    colones[j]++;
+                }
             }
         }
-        return index;
-    }
-
-    bool checkColumn(std::vector<std::vector<int>>& mat, int i, int index) {
-        for (int j = 0; j < mat.size(); j++) {
-            if (mat[j][index] == 1 && j != i)
-                return false;
+        int result=0;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(mat[i][j]==1){
+                    if(rowones[i]==1&&colones[j]==1) result++;
+                }
+            }
         }
-        return true;
+        return result;
     }
 };
