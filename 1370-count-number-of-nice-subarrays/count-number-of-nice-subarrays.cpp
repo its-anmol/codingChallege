1bea45fbc035c8d9cbacaf230b13cf4586644a75
Anmol Srivastava <106250049+its-anmol@users.noreply.github.com>
2024-03-14 10:55:45 +0530

    Time: 159 ms (32.33%) | Memory: 93.7 MB (33.92%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        return atmost(nums,k)-atmost(nums,k-1);
-    }
-    int atmost(vector<int>&nums,int k){
+        unordered_map<int,int> umap;
         int count=0;
-        int end=0;
-        int start=0;
-        int ans=0;
-        while(end<nums.size()){
-            if(nums[end]%2!=0) count++;
-            while(start<=end && count>k){
-                if(nums[start]%2!=0){
-                    count--;
-                }
-                start++;
+        int res=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2!=0){
+                count++;
             }
-            ans+=(end-start+1);
-            end++;
+            if(count==k){
+                res++;
+            }
+            if(umap.find(count-k)!=umap.end()){
+                res+=umap[count-k];
+            }
+
+            umap[count]++;
         }
-        return ans;
+        return res;
+
     }
 };
