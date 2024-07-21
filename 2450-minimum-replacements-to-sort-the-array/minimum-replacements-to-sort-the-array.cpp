fbdcda035b6451d94b4683344a739105d35a66d6
Anmol Srivastava <106250049+its-anmol@users.noreply.github.com>
2024-07-21 15:48:27 +0530

    Time: 82 ms (73.55%) | Memory: 57.3 MB (14.51%) - LeetSync

--- 2450-minimum-replacements-to-sort-the-array/minimum-replacements-to-sort-the-array.cpp
+++ 2450-minimum-replacements-to-sort-the-array/minimum-replacements-to-sort-the-array.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     long long minimumReplacement(vector<int>& nums) {
+        int n=nums.size()-1;
         long long operation=0;
-        for(int i=nums.size()-2;i>=0;i--){
-            if(nums[i]<=nums[i+1]) continue;
-            int parts=nums[i]/nums[i+1];
-            if(nums[i]%nums[i+1]!=0){
-                parts++;
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]>nums[i+1]){
+                int parts=nums[i]/nums[i+1];
+                if(nums[i]%nums[i+1]!=0){
+                    parts++;
+                }
+                operation+=(parts-1);
+                nums[i]=nums[i]/parts;
             }
-            operation+=(parts-1);
-            nums[i]=nums[i]/parts;
         }
         return operation;
     }
