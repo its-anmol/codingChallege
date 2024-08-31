4e0936d6ead5c42e5814480e5c529d7420a69828
Anmol Srivastava <106250049+its-anmol@users.noreply.github.com>
2024-08-31 13:01:30 +0530

    Time: 226 ms (38.87%) | Memory: 294.9 MB (19.60%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
+    int helper(vector<int>&nums,int ind,int prevind,vector<vector<int>> &dp){
+        if(ind>=nums.size()) return 0;
+        int nottake=helper(nums,ind+1,prevind,dp);
+        int take=0;
+        if(dp[ind][prevind+1]!=-1) return dp[ind][prevind+1];
+        if(prevind==-1||nums[prevind]<nums[ind]){
+            take=1+helper(nums,ind+1,ind,dp);
+        }
+        return dp[ind][prevind+1]=max(take,nottake);
+    }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n,1);
-        int maxi=1;
-        for(int ind=1;ind<n;ind++){
-            for(int prevind=0;prevind<ind;prevind++){
-                if(nums[ind]>nums[prevind]){
-                    dp[ind]=max(dp[ind],1+dp[prevind]);
-                }
+        // vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        // return helper(nums,0,-1,dp);
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        for(int ind=n-1;ind>=0;ind--){
+            for(int prevind=ind-1;prevind>=-1;prevind--){
+                int nottake=dp[ind+1][prevind+1];
+                int take=0;
+                if(prevind==-1||nums[prevind]<nums[ind]){
+                    take=1+dp[ind+1][ind+1];
+             }
+            dp[ind][prevind+1]=max(take,nottake);
             }
-            maxi=max(maxi,dp[ind]);
         }
-        return maxi;
+        return dp[0][0];
     }
-}; 
+};
