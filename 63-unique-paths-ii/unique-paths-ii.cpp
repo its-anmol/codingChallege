b6af6c4dc46d1abbad211582eaf8a7a6a7cec5b5
Anmol Srivastava <106250049+its-anmol@users.noreply.github.com>
2024-09-06 23:05:04 +0530

    Time: 6 ms (20.62%) | Memory: 10.6 MB (38.91%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
-    int helper(vector<vector<int>>& obstacleGrid,int m,int n, vector<vector<int>> &dp){
-        if(m==0&& n==0) return 1;
-        if(m<0||n<0) return 0;
-        if(m>=0 && n>=0&&obstacleGrid[m][n]==1) return 0;
-        if(dp[m][n]!=-1) return dp[m][n];
-        int up=helper(obstacleGrid,m-1,n,dp);
-        int down=helper(obstacleGrid,m,n-1,dp);
-        return dp[m][n]=up + down;
+    int helper(vector<vector<int>>& obstacleGrid,int m,int n){
+        if(m==0||n==0) return 0;
+        if(obstacleGrid[m-1][n-1]==1) return 0;
+        if(m==1&&n==1) return 1;
+        int up=helper(obstacleGrid,m-1,n);
+        int left=helper(obstacleGrid,m,n-1);
+        return up+left;
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m=obstacleGrid.size();
         int n=obstacleGrid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) // If the starting or ending point is blocked, return 0
-            return 0;
-        return helper(obstacleGrid,m-1,n-1,dp);
+        // return helper(obstacleGrid,m,n);
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return 0;
+        vector<vector<long long>> dp(m+1,vector<long long>(n+1,0));
+        dp[1][1]=1;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(i==1&&j==1) continue;
+                if(obstacleGrid[i-1][j-1]==1){
+                    dp[i][j]=0;
+                    continue;
+                }
+                int up=dp[i-1][j];
+                int left=dp[i][j-1];
+                dp[i][j]=up+left;
+            }
+        }
+        return dp[m][n];
     }
 };
