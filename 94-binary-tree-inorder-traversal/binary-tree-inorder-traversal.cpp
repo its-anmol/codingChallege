7370296ce9ad1380f78dbe6901265b36bafee1be
Anmol Srivastava <106250049+its-anmol@users.noreply.github.com>
2024-09-11 21:54:53 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (26.53%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -20,7 +20,21 @@ public:
     }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> res;
-        inorder(root,res);
-        return res; 
+        stack<TreeNode*>st;
+        TreeNode*node=root;
+        while(true){
+            if(node){
+                st.push(node);
+                node=node->left;
+            }
+            else{
+                if(st.empty()) break;
+                node=st.top();
+                st.pop();
+                res.push_back(node->val);
+                node=node->right;
+            }
+        }
+        return res;
     }
 };
