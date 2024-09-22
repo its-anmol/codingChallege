8a20c3ecadf982d9d8377f530c4ff6ffc4a0de67
Anmol Srivastava <106250049+its-anmol@users.noreply.github.com>
2024-09-23 03:15:08 +0530

    Time: 228 ms (37.56%) | Memory: 294.7 MB (34.01%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    int helper(vector<int>&nums,int ind,int prevind,vector<vector<int>> &dp){
-        if(ind>=nums.size()) return 0;
-        int nottake=helper(nums,ind+1,prevind,dp);
-        int take=0;
-        if(dp[ind][prevind+1]!=-1) return dp[ind][prevind+1];
-        if(prevind==-1||nums[prevind]<nums[ind]){
-            take=1+helper(nums,ind+1,ind,dp);
+    int helper(vector<int>&arr,int ind,int prevind){
+        if(ind==arr.size()){
+            return 0;
         }
-        return dp[ind][prevind+1]=max(take,nottake);
+        int nottake=helper(arr,ind+1,prevind);
+        int take=0;
+        if(prevind==-1||arr[ind]>arr[prevind]){
+            take= 1+helper(arr,ind+1,ind);
+        }
+        return max(take,nottake);
     }
-    int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        // vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        // return helper(nums,0,-1,dp);
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-        for(int ind=n-1;ind>=0;ind--){
-            for(int prevind=ind-1;prevind>=-1;prevind--){
-                int nottake=dp[ind+1][prevind+1];
+    int lengthOfLIS(vector<int>& arr) {
+        // return helper(nums,0,-1);
+        int n = arr.size();
+        if (n == 0) return 0;
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int prevind = ind - 1; prevind >= -1; prevind--) {
+                int notTake=dp[ind+1][prevind+1];
                 int take=0;
-                if(prevind==-1||nums[prevind]<nums[ind]){
+                if(prevind==-1||arr[ind]>arr[prevind]){
                     take=1+dp[ind+1][ind+1];
-             }
-            dp[ind][prevind+1]=max(take,nottake);
+                }
+                dp[ind][prevind+1]=max(take,notTake);
             }
         }
         return dp[0][0];
